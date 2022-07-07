b43b82d1b67d3097e33b405c3a3329353f9144dc
pankaj <pankaj.codesdot@gmail.com>
2022-07-07 12:25:33 +0530

    Mouse Position count when hover on image, Show and hide fotter button, data binding and two way data binding live char count

--- EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
+++ EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
@@ -1,6 +1,7 @@
 ﻿using EmployeeManagement.Models;
 using EmployeeManagement.Web.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,8 @@ namespace EmployeeManagement.Web.Pages
        
             public Employee Employee { get; set; } = new Employee();
 
+           protected string Cooradinates { get; set; }
+
             [Inject]
             public IEmployeeService EmployeeService { get; set; }
 
@@ -24,7 +27,29 @@ namespace EmployeeManagement.Web.Pages
                 Id = Id ?? "1";
                 Employee = await EmployeeService.GetEmployee(int.Parse(Id));
             }
-        
+
+        //  protected void Mouse_Move(MouseEventArgs e)
+        //{
+        //    Cooradinates = $"X={e.ClientX} Y={e.ClientY}";
+        //}
+        //we use logic in img tag with lambda expression
+
+        protected string ButtonText { get; set; } = "Hide Footer";
+        protected string CssClass { get; set; } = null;
+
+        protected void Button_Click()
+        {
+            if (ButtonText == "Hide Footer")
+            {
+                ButtonText = "Show Footer";
+                CssClass = "HideFooter";
+            }
+            else
+            {
+                CssClass = null;
+                ButtonText = "Hide Footer";
+            }
+        }
     }
 }
 
